ec3ecb90e817241348996f4844886f5204807156
mng <>
2021-03-29 18:19:19 +0800

    baby arrived

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as React from "react";
+
+const colors = [
+  "#ff5733",
+  "#33ffc3",
+  "#fd9bdb",
+  "#9bcffd",
+  "#fdfa9b",
+  "#eb5f45",
+  "#91eb45",
+  "#45ebe4",
+  "#b545eb",
+];
 
 function App() {
+  const [value, setValue] = React.useState(0);
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((v) => (v === 9 ? 0 : v + 3));
+    }, 1000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header style={{ ...appHeader, background: colors[value] }}>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Where is baby Houston?
-        </p>
-        <p>...still baking</p>
+        <p>Whhooopp whooopp</p>
+        <p>Baby Houston is here!!!</p>
       </header>
     </div>
   );
 }
+
+
+const appHeader={
+    minHeight: '100vh',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 'calc(10px + 2vmin)'
+  }
 
 export default App;
